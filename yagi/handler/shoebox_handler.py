c1715505ca3f9bff397d5c4e01b7e76e182e3b75
Sandy Walsh <sandy@sandywalsh.com>
2014-05-27 21:09:48 +0000

    shoebox handler working. Need to remove some debug stuff

--- yagi/handler/shoebox_handler.py
+++ yagi/handler/shoebox_handler.py
@@ -1,3 +1,7 @@
+import json
+import os
+
+import notification_utils
 from shoebox import roll_manager
 import simport
 
@@ -19,21 +23,26 @@ class ShoeboxHandler(yagi.handler.BaseHandler):
         super(ShoeboxHandler, self).__init__(app, queue_name)
         # Don't use interpolation from ConfigParser ...
         self.config = dict(yagi.config.config.items('shoebox', raw=True))
-        roll_checker = self.config['roll_checker']
-        self.roll_checker = simport.load(roll_checker)(**self.config)
+        roll_checker_str = self.config['roll_checker']
+        self.roll_checker = simport.load(roll_checker_str)(**self.config)
         self.working_directory = self.config.get('working_directory', '.')
+        self.destination_folder = self.config.get('destination_folder', '.')
+        for d in [self.working_directory, self.destination_folder]:
+            if not os.path.isdir(d):
+                os.makedirs(d)
         template=self.config.get('filename_template',
                                  'events_%Y_%m_%d_%X_%f.dat')
         cb = simport.load(self.config['callback'])(config=self.config)
         self.roll_manager = roll_manager.WritingRollManager(template,
-                                roll_checker, self.working_directory,
+                                self.roll_checker, self.working_directory,
                                 archive_callback=cb)
 
     def handle_messages(self, messages, env):
         # TODO(sandy): iterate_payloads filters messages first ... not
         # sure if we want that for raw archiving.
         for payload in self.iterate_payloads(messages, env):
-            json_event = json.dumps(event,
+            metadata = {}
+            json_event = json.dumps(payload,
                                     cls=notification_utils.DateTimeEncoder)
-
-            manager.write(metadata, json_event)
+            print ">", payload
+            self.roll_manager.write(metadata, json_event)
